d73c7c5766044ed6a2d4303ced30247b68aa4308
husainsaify <hackerkernel15@gmail.com>
2016-02-15 15:06:12 +0530

    Final touchup

--- app/src/main/java/com/murtaza/navigation1/network/ApiUrl.java
+++ app/src/main/java/com/murtaza/navigation1/network/ApiUrl.java
@@ -1,6 +1,6 @@
 package com.murtaza.navigation1.network;
 
 public class ApiUrl {
-    private static String SERVER = "http://demo.hackerkernel.com/mCent/";
+    private static String SERVER = "http://earntalktimeapp.com/api/";
     public static String OFFER_LIST = SERVER + "index.php";
 }
